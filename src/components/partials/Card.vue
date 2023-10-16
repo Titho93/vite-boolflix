3a6be98cec965553bb73d95c4ed2b4b35adb1b9f
titho93 <titho93.10@gmail.com>
2023-10-17 01:12:42 +0200

    [edit]hover card

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -22,7 +22,7 @@ export default {
         <img v-else :src="'https://image.tmdb.org/t/p/w342/' + image"  :alt=" title ">
 
       </div>
-      
+
       <div class="bio">
         
         <h5>{{ title }}</h5>
@@ -40,12 +40,39 @@ export default {
 .card-bt{
   text-align: center;
   cursor: pointer;
+  position: relative;
+  box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5); 
+  margin: 20px 10px;
+  border: 1px solid white;
+  z-index: 2;
 
   img {
     width: 100%;
     height: 180px;
   }
+  
+  .bio {
+    width: 100%;
+    height: 100%;
+    position: absolute; 
+    top: 0px;
+    display: none;
+    
+  }
+
 }
 
+.card-bt:hover {
+  .cover {
+    opacity: 0.3;
+  }
+
+  .bio {
+    display: block;
+    opacity: 1;
+    z-index: 3;
+    color: white;
+  }
+}
 
 </style>
